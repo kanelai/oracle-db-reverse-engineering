7eb69b5eb1337d0c74242b4f45baf8af0761a6bb
Kane Lai <kanehh.lai@gmail.com>
2015-11-29 13:15:39 +0800

    First check-in

---
+++ exptab_test.sql
@@ -0,0 +1,27 @@
+drop table exptab_test;
+
+create table exptab_test(
+	a binary_float,
+	b binary_double,
+	c date,
+	d timestamp(6),
+	e raw(100),
+	f blob,
+	g bfile,
+	h varchar2(23),
+	i nvarchar2(55),
+	j char,
+	k nchar,
+	l long,
+	m clob,
+	n nclob,
+	o number,
+	p number(5,0),
+	q number(12,0),
+	r number(28,0),
+	s number(5,8),
+	t number(12,15),
+	u number(29,30),
+	v number(5),
+	w number(28));
+
